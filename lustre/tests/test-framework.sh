b4b36852f40e5365ea21341a0f9929cdf7e1be7d
Oleg Drokin <oleg.drokin@intel.com>
2018-04-03 23:52:11 -0400

    Revert "LU-10835 tests: unload dm-flakey module"
    This reverts commit 29e3752eb7d44cded146c124f7bed6dc35fa38db.

--- lustre/tests/test-framework.sh
+++ lustre/tests/test-framework.sh
@@ -1830,9 +1830,6 @@ dm_cleanup_dev() {
 
 	# detach a loop device
 	[[ $major -ne 7 ]] || cleanup_loop_device $facet /dev/loop$minor
-
-	# unload dm-flakey module
-	do_facet $facet "modprobe -r dm-flakey" || true
 }
 
 mount_facet() {
